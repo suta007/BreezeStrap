477fd271efcfef698b0a3907189482cc9e78dde0
Kritsadapong  Suta <mr.suta@gmail.com>
2022-10-02 09:44:45 +0700

    update comnposer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '90ed6ca8ee03be7ca9896a435bb2ba3e9d33cd4c',
+        'name' => 'suta007/breezestrap',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'suta007/breezestrap' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '90ed6ca8ee03be7ca9896a435bb2ba3e9d33cd4c',
+            'dev_requirement' => false,
+        ),
+    ),
+);
